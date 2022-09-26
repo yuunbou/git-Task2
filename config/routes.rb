693c0d9c70fe8b829e1a1b4453c5180ca9f995ca
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-09-26 17:06:59 +0000

    [update]Task2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  resources :books
+  
+  
+  
 end
