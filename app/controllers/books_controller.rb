c1a813e327dda634b3f4901c04e216edc8d61419
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-09-27 05:00:14 +0000

    [add]Task2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,18 @@
 class BooksController < ApplicationController
   
-  def new
-    @book = Books.new
+  def index
+    @books = Book.all
+    @book = Book.new(book_params)
   end
   
   def create
     @book = Book.new(book_params)
-    
-  end
-  
-  def index
-    @books = Book.all
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+    flash[:notice] = "successfully"
   end
   
   def show
@@ -25,12 +27,21 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
+    flash[:notice] = "successfully"
   end
-   
   
+  def destory
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
+    flash[:notice] = "successfully"  
+    end
+   
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
+   
+   
   
 end
