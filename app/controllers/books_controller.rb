470b776e2a5c342d9d3aa5d0adc212de68739ad1
yuunbou <112618391+yuunbou@users.noreply.github.com>
2022-09-28 07:23:05 +0000

    [Finish]Task2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "successfully"
+      flash[:notice] = "Book was successfully create."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -26,17 +26,20 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
-    flash[:notice] = "successfully"
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully update"
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
   
-  def destory
+  def destroy
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to '/books'
-    flash[:notice] = "successfully"
+    flash[:notice] = "Book was successfully destroyed."
   end
    
   private
@@ -45,5 +48,4 @@ class BooksController < ApplicationController
   end
    
    
-  
 end
